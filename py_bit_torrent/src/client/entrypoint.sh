54e8235606f9b5504b335a4971192e126f24f062
Eisler <eisler.valles@katapulk.com>
2024-12-01 16:07:23 -0500

    New Docker Config

--- py_bit_torrent/src/client/entrypoint.sh
+++ py_bit_torrent/src/client/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 ip route add 10.0.11.0/24 via 10.0.10.2 dev eth0
-python client.py
+python /app/py_bit_torrent/src/client/client.py
 tail -f /dev/null
--- py_bit_torrent/src/server/entrypoint.sh
+++ py_bit_torrent/src/server/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 ip route add 10.0.10.0/24 via 10.0.11.2 dev eth0
-python server.py
+python /app/py_bit_torrent/src/server/server.py
 tail -f /dev/null
